54286eb5158d48e88dcf264def95bf27d77dbf81
Cody Fingerson <codyfingerson0428@gmail.com>
2024-10-29 00:49:17 -0600

    Refactor code to add comments and documentation

--- parser/Statement.ts
+++ parser/Statement.ts
@@ -1,8 +1,62 @@
 import type { Token } from "../scanner/Token";
 import { Expression } from "./Expression";
 
+/**
+ * Represents a statement in the language.
+ * @abstract
+ */
 export abstract class Statement {}
 
+/**
+ * Represents a function statement
+ * 
+ * @example
+ * ```
+ * function add(a, b) { 
+ *  return a + b; 
+ * }
+ * ```
+ */
+export class FunctionStatement extends Statement {
+    public name: Token;
+    public params: Token[];
+    public body: Statement[];
+
+    constructor(name: Token, params: Token[], body: Statement[]) {
+        super();
+        this.name = name;
+        this.params = params;
+        this.body = body;
+    }
+}
+
+/**
+ * Represents a return statement, i.e: 
+ * 
+ * @example
+ * ```
+ * return 1;
+ * ```
+ */
+export class ReturnStatement extends Statement {
+    public keyword: Token;
+    public value: Expression | null;
+
+    constructor(keyword: Token, value: Expression | null) {
+        super();
+        this.keyword = keyword;
+        this.value = value;
+    }
+}
+
+/**
+ * Represents an output statement that prints a value to the console.
+ * 
+ * @example
+ * ```
+ * output 1;
+ * ```
+ */
 export class OutputStatement extends Statement {
     public expression: Expression;
 
@@ -16,6 +70,16 @@ export class OutputStatement extends Statement {
     }
 }
 
+/**
+ * Represents a print statement
+ * 
+ * @example
+ * ```
+ * if(true) {
+ *  output 1;
+ * }
+ * ```
+ */
 export class IfStatement extends Statement {
     public condition: Expression;
     public thenBranch: Statement;
@@ -34,6 +98,14 @@ export class IfStatement extends Statement {
     }
 }
 
+/**
+ * Expression statement
+ * 
+ * @example
+ * ```
+ * 1 + 2;
+ * ```
+ */
 export class ExpressionStatement extends Statement {
     public expression: any;
 
@@ -43,6 +115,14 @@ export class ExpressionStatement extends Statement {
     }
 }
 
+/**
+ * Represents a variable declaration statement
+ * 
+ * @example
+ * ```
+ * create a = 1;
+ * ```
+ */
 export class CreateStatement extends Statement {
     public name: Token;
     public initializer: Expression | null;
@@ -58,6 +138,9 @@ export class CreateStatement extends Statement {
     }
 }
 
+/**
+ * Represents a block statement
+ */
 export class BlockStatement extends Statement {
     public statements: Statement[];
 
@@ -68,5 +151,26 @@ export class BlockStatement extends Statement {
 
     public toString(): string {
         return `BlockStatement { statements: ${this.statements} }`;
+    }
+}
+
+/**
+ * Represents a while statement
+ * 
+ * @example
+ * ```
+ * while(true) {
+ *  output 1;
+ * }
+ * ```
+ */
+export class WhileStatement extends Statement {
+    public condition: Expression;
+    public body: Statement;
+
+    constructor(condition: Expression, body: Statement) {
+        super();
+        this.condition = condition;
+        this.body = body;
     }
 }
